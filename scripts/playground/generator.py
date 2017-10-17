8e1dd4eaa3a45f8d9998e9dd9e1ab093dec90db0
Rohithyeravothula <rohithiitj@gmail.com>
2017-10-17 12:28:48 -0700

    adds test cases and generator scripts

--- scripts/playground/generator.py
+++ scripts/playground/generator.py
@@ -1,6 +1,18 @@
-import sys
+import time, sys, os
 import math
 import random
+
+def gravity(a):
+	n=len(a)
+	for j in range(0,n):
+		b = []
+		for i in range(0,n):
+			if a[i][j] != -1:
+				b.append(a[i][j])
+		# print(b)
+		b = [-1]*(n-len(b)) + b
+		for i in range(0, n):
+			a[i][j] = b[i]
 
 
 def generate_test(size, fruits, fileName):
@@ -14,7 +26,7 @@ def generate_test(size, fruits, fileName):
 	while count < maxC:
 		i = random.randint(0, size-1)
 		j = random.randint(0, size-1)
-		f = random.randint(0, int(math.sqrt(fruits))-1)
+		f = random.randint(-1, int(math.sqrt(fruits)))
 		if a[i][j] == -1:
 			a[i][j] = f
 
@@ -26,20 +38,45 @@ def generate_test(size, fruits, fileName):
 				a[i][j] = random.randint(0, fruits)
 
 	# print(len(a[0]), len(a))
-	write_to_file(fileName, size, fruits, 12, a)
+	gravity(a)
+	time = (random.random() + 0.5) * (size**2)
+	# print(a)
+	write_to_file(fileName, size, fruits, time, a)
 
 def write_to_file(fileName, size, fruits_count, time, a):
 	ary = str(size) + "\n" + str(fruits_count) + "\n" + str(time) + "\n"
 	for i in range(0, size):
 		for j in range(0, size):
-			ary += str(a[i][j])
+			if(a[i][j] == -1):
+				ary += "*"
+			else:
+				ary += str(a[i][j])
 		ary += "\n"
 	ary = ary[:-1]
+	# print(ary)
 	f = open(fileName, 'w')
 	f.write(ary)
 	f.close()
+
+
+def randomTest():
+	for i in range(0, 100):
+		generate_test(random.randint(1, 26), random.randint(0, 9), "data/input.txt")
+		time.sleep(1)
+		os.system('clear')
+
+
+def defined_test():
+	n = [10, 15, 20, 26]
+	f = [2, 6, 9]
+	for i in n:
+		for j in f:
+			generate_test(i, j, "data/input" + str(i) + "-" + str(j) + " .txt")
+
+
 
 if __name__ == '__main__':
 	nInp=int(sys.argv[1])
 	fInp=int(sys.argv[2])
 	generate_test(nInp, fInp, "data/input.txt")
+	# defined_test()
--- scripts/playground/loop.sh
+++ scripts/playground/loop.sh
@@ -1,5 +1,33 @@
-for i in {1..100}
-do
-python3 generator.py 5 3
+#for i in {1..40}
+#do
+#python3thon3 generator.py 8 4
+#./fight.sh
+#done
+
+python3 generator.py 20 9 
 ./fight.sh
-done
+python3 generator.py 15 2
+./fight.sh
+python3 generator.py 15 5 
+./fight.sh
+python3 generator.py 15 9 
+./fight.sh
+python3 generator.py 10 2
+./fight.sh
+python3 generator.py 10 5 
+./fight.sh
+python3 generator.py 10 9 
+./fight.sh
+python3 generator.py 12 2 
+./fight.sh
+python3 generator.py 12 5 
+./fight.sh
+python3 generator.py 12 9 
+./fight.sh
+python3 generator.py 25 2 
+./fight.sh
+python3 generator.py 25 5 
+./fight.sh
+python3 generator.py 25 9 
+./fight.sh
+
